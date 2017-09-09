f54c2620854eb977652624d53ecfe7b77018c7f1
ceech <ceech@users.noreply.github.com>
2017-09-09 15:06:29 +0200

    Add files via upload

---
+++ AC_dimmer.ino
@@ -0,0 +1,64 @@
+/*
+
+AC Voltage dimmer with Zero cross detection
+Author: Charith Fernanado <a href="http://www.inmojo.com">  http://www.inmojo.com
+
+</a>
+Adapted by DIY_bloke
+License: Creative Commons Attribution Share-Alike 3.0 License.
+Attach the Zero cross pin of the module to Arduino External Interrupt pin
+Select the correct Interrupt # from the below table 
+(the Pin numbers are digital pins, NOT physical pins: 
+digital pin 2 [INT0]=physical pin 4 and digital pin 3 [INT1]= physical pin 5)
+check: <a href="http://arduino.cc/en/Reference/attachInterrupt">  http://www.inmojo.com
+
+</a>
+
+Pin    |  Interrrupt # | Arduino Platform
+---------------------------------------
+2      |  0            |  All -But it is INT1 on the Leonardo
+3      |  1            |  All -But it is INT0 on the Leonardo
+18     |  5            |  Arduino Mega Only
+19     |  4            |  Arduino Mega Only
+20     |  3            |  Arduino Mega Only
+21     |  2            |  Arduino Mega Only
+0      |  0            |  Leonardo
+1      |  3            |  Leonardo
+7      |  4            |  Leonardo
+The Arduino Due has no standard interrupt pins as an iterrupt can be attached to almosty any pin. 
+
+In the program pin 2 is chosen
+*/
+int AC_LOAD = 10;    // Output to Opto Triac pin
+int dimming = 128;  // Dimming level (0-128)  0 = ON, 128 = OFF
+
+void setup()
+{
+  pinMode(AC_LOAD, OUTPUT);// Set AC Load pin as output
+  attachInterrupt(0, zero_crosss_int, RISING);  // Choose the zero cross interrupt # from the table above
+}
+
+//the interrupt function must take no parameters and return nothing
+void zero_crosss_int()  //function to be fired at the zero crossing to dim the light
+{
+  // Firing angle calculation : 1 full 50Hz wave =1/50=20ms 
+  // Every zerocrossing thus: (50Hz)-> 10ms (1/2 Cycle) 
+  // For 60Hz => 8.33ms (10.000/120)
+  // 10ms=10000us
+  // (10000us - 10us) / 128 = 75 (Approx) For 60Hz =>65
+
+  int dimtime = (75*dimming);    // For 60Hz =>65    
+  delayMicroseconds(dimtime);    // Wait till firing the TRIAC    
+  digitalWrite(AC_LOAD, HIGH);   // Fire the TRIAC
+  delayMicroseconds(10);         // triac On propogation delay 
+         // (for 60Hz use 8.33) Some Triacs need a longer period
+  digitalWrite(AC_LOAD, LOW);    // No longer trigger the TRIAC (the next zero crossing will swith it off) TRIAC
+}
+
+void loop()  {
+  for (int i=5; i <= 128; i++){
+    dimming=i;
+    delay(10);
+   }
+}
+
